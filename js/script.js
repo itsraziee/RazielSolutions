5894915ed0f271263ace0a98a72eca99b9bb476b
itsraziee <itsrazieemaetan@gmail.com>
2022-12-13 22:01:44 +0800

    portfolio

--- js/script.js
+++ js/script.js
@@ -29,3 +29,53 @@ for (var i = 0; i < navLinks.length; i++) {
     menuBtn.style.pointerEvents = "auto";
   });
 }
+
+// --------------------Portfolio-------------------------
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+// ---------------------------Web Design---------------------------
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+// ---------------------------Web Development---------------------------
+let slideIndex2 = 0;
+showSlides2();
+
+function showSlides2() {
+  let j;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  for (j = 0; j < slides2.length; j++) {
+    slides2[j].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {
+    slideIndex2 = 1;
+  }
+  slides2[slideIndex2 - 1].style.display = "block";
+  setTimeout(showSlides2, 3000); // Change image every 2 seconds
+}
